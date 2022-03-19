59f45c34c0fd599e69a76e122b7a2eb2e87796f4
OwnKng <owen.king@outlook.com>
2022-03-19 21:16:19 +0000

    Initial effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//@ts-nocheck
+import React from "react"
+import { useCanvas } from "./hooks/useCanvas"
 
-function App() {
+const App = () => {
+  const { canvasRef, canvasWidth, canvasHeight, onMouseMove } = useCanvas()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas
+        onMouseMove={(event) => onMouseMove(event)}
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/hooks/useCanvas.tsx
@@ -0,0 +1,66 @@
+// @ts-nocheck
+import { useRef, useEffect } from "react"
+
+const canvasWidth = window.innerWidth
+const canvasHeight = window.innerHeight
+
+const update = (coord) => ({
+  ...coord,
+  size: coord.size <= 0 ? 0 : coord.size - 1,
+})
+
+export const useCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null!)
+
+  const coordsRef = useRef([])
+
+  const requestRef = useRef()
+
+  const onMouseMove = (event: any) => {
+    const currentCoords = {
+      x: event.clientX,
+      y: event.clientY,
+      age: 0,
+      size: 32,
+    }
+
+    coordsRef.current = [...coordsRef.current, currentCoords]
+  }
+
+  const drawParticles = () => {
+    const canvasObj = canvasRef.current
+    const ctx = canvasObj.getContext("2d")
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    if (coordsRef.current.length > 200) {
+      coordsRef.current = coordsRef.current.slice(1)
+    }
+
+    if (coordsRef.current.length) {
+      coordsRef.current = coordsRef.current.map((coord) => update(coord))
+
+      for (let i = 0; i < coordsRef.current.length; i++) {
+        const { x, y, size } = coordsRef.current[i]
+        ctx.fillStyle = "black"
+        ctx.beginPath()
+        ctx.arc(x, y, size, 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.fill()
+      }
+    }
+
+    requestRef.current = requestAnimationFrame(drawParticles)
+  }
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(drawParticles)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [])
+
+  return {
+    canvasRef,
+    canvasWidth,
+    canvasHeight,
+    onMouseMove,
+  }
+}
